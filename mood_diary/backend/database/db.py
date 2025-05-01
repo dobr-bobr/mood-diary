6c5a712c77a7d685f58db11097ccb9220fe8b8bb
mihdenis85 <mihdenis777@yandex.ru>
2025-05-01 19:01:01 +0300

    Formatted

--- mood_diary/backend/database/db.py
+++ mood_diary/backend/database/db.py
@@ -1,7 +1,12 @@
 import sqlite3
 
-from mood_diary.backend.repositories.sqlite.user import SQLiteUserRepository as UserRepository
-from mood_diary.backend.repositories.sqlite.mood import SQLiteMoodRepository as MoodRepository
+from mood_diary.backend.repositories.sqlite.user import (
+    SQLiteUserRepository as UserRepository,
+)
+from mood_diary.backend.repositories.sqlite.mood import (
+    SQLiteMoodRepository as MoodRepository,
+)
+
 
 def init_db(db_path: str):
     conn = sqlite3.connect(db_path)
